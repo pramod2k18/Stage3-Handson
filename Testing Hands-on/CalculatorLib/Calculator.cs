b95b9fb5b63d83c2e7719186ea7a749d7620d12a
Pramod Kumar <pramodparkers@gmail.com>
2021-04-25 20:24:02 +0530

    Added Testing hands-on

---
+++ Testing Hands-on/CalculatorLib/Calculator.cs
@@ -0,0 +1,17 @@
+﻿namespace CalculatorLib
+{
+    public class Calculator
+    {
+        public int Add(int firstNumber,int secondNumber)
+        {
+            if (firstNumber>0&&secondNumber>0)
+            {
+                return firstNumber + secondNumber;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Testing Hands-on/CalculatorLib/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CalculatorLib
+{
+    class Class1
+    {
+        [TestFixture]
+        public class Tests
+        {
+            [Test] 
+            [TestCase(2, 2, 4)] 
+            [TestCase(0, 0, 2)] 
+            public void Add(int firstNumber, int secondNumber, int exp) { 
+                if (firstNumber > 0 && secondNumber > 0) {
+                    int r = firstNumber + secondNumber; Assert.That(r, Is.EqualTo(exp)); 
+                } 
+            }
+
+        }
+    }
+}
